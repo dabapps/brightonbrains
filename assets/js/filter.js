9d0e8d84fb6a27faebbd84baa4e0cfe62d95bbff
Jake 'Sid' Smith <jake@dabapps.com>
2018-11-30 17:35:12 +0000

    Load existing filter from query

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,6 +1,7 @@
 'use strict';
 
 (function() {
+  var MATCHES_FILTER_QUERY = /\?filter=(.+)/;
   var DISPLAY_NONE = 'display-none';
 
   var filter;
@@ -15,8 +16,7 @@
     el.classList.add(DISPLAY_NONE);
   }
 
-  function onInputChange(e) {
-    var searchValue = e.target.value.toLowerCase();
+  function updateListingsVisibility(searchValue) {
     if (!searchValue) {
       Array.prototype.forEach.call(listings, setActiveState);
     } else {
@@ -30,6 +30,11 @@
     }
   }
 
+  function onInputChange(e) {
+    var searchValue = e.target.value.toLowerCase();
+    updateListingsVisibility(searchValue);
+  }
+
   function init() {
     filterForm = document.getElementById('filter-form');
 
@@ -38,8 +43,14 @@
     }
 
     filter = document.getElementById('filter');
-
     listings = document.getElementsByClassName('listing');
+
+    var filterQuery = MATCHES_FILTER_QUERY.exec(location.search);
+
+    if (filterQuery) {
+      filter.value = filterQuery[1].toLowerCase();
+      updateListingsVisibility(filter.value);
+    }
 
     filterForm.classList.remove(DISPLAY_NONE);
     filter.addEventListener('input', onInputChange);
